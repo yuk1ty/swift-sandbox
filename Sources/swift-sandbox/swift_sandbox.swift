665dbd7839d50d4138ccaaa60e7f62d2071a213f
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-08-14 15:29:14 +0900

    first commit

--- Sources/swift-sandbox/swift_sandbox.swift
+++
@@ -1,3 +0,0 @@
-struct swift_sandbox {
-    var text = "Hello, World!"
-}
--- Tests/swift-sandboxTests/XCTestManifests.swift
+++ Tests/swift-sandboxTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(swift_sandboxTests.allTests),
-    ]
-}
-#endif
+//import XCTest
+//
+//#if !canImport(ObjectiveC)
+//public func allTests() -> [XCTestCaseEntry] {
+//    return [
+//        testCase(swift_sandboxTests.allTests),
+//    ]
+//}
+//#endif
