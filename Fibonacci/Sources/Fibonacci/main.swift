dbc85e7f1a087efa4c95e08830bf77a3c988b5ac
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-08-14 18:37:34 +0900

    add fibonacci

---
+++ Fibonacci/Sources/Fibonacci/main.swift
@@ -0,0 +1,17 @@
+func fibonacci(n: Int) -> Int {
+    if n == 0 || n == 1 {
+        return n
+    } else {
+        return fibonacci(n: n - 1) + fibonacci(n: n - 2)
+    }
+}
+
+func fibonacci_with_guard(n: Int) -> Int {
+    guard n != 0, n != 1 else {
+        return n
+    }
+    return fibonacci(n: n - 1) + fibonacci(n: n - 2)
+}
+
+assert(fibonacci(n: 7) == 13)
+assert(fibonacci_with_guard(n: 7) == 13)
